23537e5b151f455ac3389d880e1a0477506a583b
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-10-17 20:32:53 -0400

    React Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="emoji-logo" role="img" aria-label='paper'>📃</span>
+      <h1>Preeepare</h1>
+    </div>
+  );
+}
+
+export default App;
