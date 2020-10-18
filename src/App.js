7a218ee3d144a3d2415cdde3a941878a1e9c636a
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-10-17 21:15:31 -0400

    added title and link to github page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <span className="emoji-logo" role="img" aria-label='paper'>📃</span>
+      <a href='#temp' target='_blank'>
+        <button className="to-code">
+          <span className="emoji-logo" role="img" aria-label='paper'>📃</span>
+        </button>
+      </a>
+
       <h1>Preeepare</h1>
+      <h2>Visualize your progression</h2>
     </div>
   );
 }
